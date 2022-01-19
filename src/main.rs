3bcd852d469968ef2a88b90ae9a9d3652b21de11
latentd <latentd0@gmail.com>
2022-01-19 16:08:32 +0900

    add feature decode

--- src/main.rs
+++ src/main.rs
@@ -115,16 +115,108 @@ fn encode_test_vectors_foobar_rfc4648() {
     assert_eq!(base64_encode(input.to_string()), want);
 }
 
+fn strip_padding(input: String) -> String {
+    input.trim_end_matches("=").to_string()
+}
+
+fn reverse_render_sextets(text: String) -> Vec<u8> {
+    text
+    .chars()
+    .map(|c| {
+        INDEX_TABLE.iter().position(|v| v == &c).unwrap()
+    })
+    .map(|c| c as u8 )
+    .collect::<Vec<u8>>()
+}
+
+fn reverse_convert_to_sextets(sextets: Vec<u8>) -> String {
+    let mut decoded = String::new();
+    for chunk in sextets.chunks(4).collect::<Vec<&[u8]>>() {
+        let length = chunk.len();
+        let mut bits: u32 = 0;
+        for i in 0..length {
+            bits |= (chunk[i] as u32) << (3-i)*6;
+        }
+        let bytes = bits.to_be_bytes()[1..length].to_vec();
+        decoded.push_str(std::str::from_utf8(&bytes).unwrap());
+    };
+    decoded
+}
+
+fn base64_decode(input: String) -> String {
+    let stripped = strip_padding(input);
+    let sextets = reverse_render_sextets(stripped);
+    reverse_convert_to_sextets(sextets)
+}
+
+#[test]
+fn decode_test_vectors_empty_rfc4648() {
+    let input = "";
+    let want = "";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
+#[test]
+fn decode_test_vectors_f_rfc4648() {
+    let input = "Zg==";
+    let want = "f";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
+#[test]
+fn decode_test_vectors_fo_rfc4648() {
+    let input = "Zm8=";
+    let want = "fo";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
+#[test]
+fn decode_test_vectors_foo_rfc4648() {
+    let input = "Zm9v";
+    let want = "foo";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
+#[test]
+fn decode_test_vectors_foob_rfc4648() {
+    let input = "Zm9vYg==";
+    let want = "foob";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
+#[test]
+fn decode_test_vectors_fooba_rfc4648() {
+    let input = "Zm9vYmE=";
+    let want = "fooba";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
+#[test]
+fn decode_test_vectors_foobar_rfc4648() {
+    let input = "Zm9vYmFy";
+    let want = "foobar";
+    assert_eq!(base64_decode(input.to_string()), want);
+}
+
 /// Base64 encode/decode util
 #[derive(Parser)]
 struct Cli {
     /// Input text
     input: String,
+    /// Decode Mode
+    #[clap(short, long)]
+    decode: bool,
 }
 
 fn main() {
     setup_panic!();
 
     let args = Cli::parse();
-    println!("{}", base64_encode(args.input));
+    let result;
+    if args.decode {
+        result = base64_decode(args.input)
+    } else {
+        result =base64_encode(args.input)
+    }
+    println!("{}", result);
 }
